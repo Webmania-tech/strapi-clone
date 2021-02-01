b2b1328504925bf55decb78b95e05d3b5ba63447
Anna Ezzarani <ask.webmania@gmail.com>
2021-02-01 20:30:00 +0100

    homepage hero title, text and backgrounds

--- index.php
+++ index.php
@@ -2,8 +2,29 @@
 
 <body>
     <?php include 'partials/header.php'; ?>
-    <section class="hero hp-hero">
-
+    <section class="hero hpHero">
+        <div class="backgrounds">
+            <div class="background-top">
+                <img src="img/bg/hp-hero-top.svg" alt="Background images geometric shapes">
+            </div>
+            <div class="background-bottom">
+                <img src="img/bg/hp-hero-bottom.svg" alt="Background images square shapes">
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="hpHero__text">
+                <h1 class="hpHero__heading">Design APIs fast, manage content easily.</h1>
+                <p class="hpHero__paragraph">Strapi is the leading open-source headless CMS. It’s 100% Javascript, fully customizable and developer-first.</p>
+                <div class="hpHero__cta">
+                    <a href="#" class="btn btn-purple">Get started</a>
+                    <a href="#" class="btn btn-yellow">Try live demo</a>
+                </div>
+            </div>
+            <div class="hpHero__illustration">
+                <img src="./img/illustrations/desktop.svg" alt="">
+                <!-- <img src="./img/illustrations/mobile.svg" alt=""> -->
+            </div>
+        </div>
     </section>
 
     <?php include 'partials/footer.php'; ?>
